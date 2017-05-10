11a25a122b81eee95f9bf2d4b579fbbffc99dbd7
Robert Grimm <rgrimm@werner.com>
2017-05-10 11:18:32 -0500

    Build out the multiple-choice question specification more to use SubjectSpek

--- base/src/test/kotlin-jvm/rmg/apps/cards/base/model/MultipleChoiceQuestionSpec.kt
+++ base/src/test/kotlin-jvm/rmg/apps/cards/base/model/MultipleChoiceQuestionSpec.kt
@@ -2,33 +2,71 @@ package rmg.apps.cards.base.model
 
 import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.reset
 import com.nhaarman.mockito_kotlin.then
-import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.subject.SubjectSpek
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
-object MultipleChoiceQuestionSpec: Spek({
+object MultipleChoiceQuestionSpec: SubjectSpek<MultipleChoiceQuestion>({
     describe("a multiple choice question") {
         val locale = Signifier.Locale(lang = "eng")
         val answerOptions = (1..5).map { WrittenWord(locale, it.toString()) }
-        val questionSignified = Signified(Signified.Type.NOUN, listOf(answerOptions[0]))
+        val correctAnswerIndex = 0
+        val incorrectAnswerIndex = 1
+        val questionSignified = Signified(Signified.Type.NOUN, listOf(answerOptions[correctAnswerIndex]))
         val handler = mock<Question.() -> Unit>()
 
-        val question = MultipleChoiceQuestion(questionSignified, answerOptions, handler)
+        beforeEachTest { reset(handler) }
 
-        on("assigning a selected index") {
-            question.selectedIndex = 0
+        subject {
+            MultipleChoiceQuestion(
+                questionSignified = questionSignified,
+                answerSignifiers = answerOptions,
+                handler = handler)
+        }
+
+        it("should not show answered initially") {
+            assertFalse { subject.isAnswered }
+        }
+
+        it("should not have any correct/incorrect value") {
+            assertNull(subject.isCorrect)
+        }
+
+        on("selecting a correct answer") {
+            subject.selectedIndex = correctAnswerIndex
 
             it("should call the handler") {
-                then(handler).should().invoke(eq(question))
+                then(handler).should().invoke(eq(subject))
             }
 
             it("should show answered") {
-                assertTrue {
-                    question.isAnswered
-                }
+                assertTrue { subject.isAnswered }
+            }
+
+            it("should indicate correct") {
+                assertTrue { subject.isCorrect!! }
+            }
+        }
+
+        on("selecting an incorrect answer") {
+            subject.selectedIndex = incorrectAnswerIndex
+
+            it("should call the handler") {
+                then(handler).should().invoke(eq(subject))
+            }
+
+            it("should show answered") {
+                assertTrue { subject.isAnswered }
+            }
+
+            it("should indicate that the answer is incorrect") {
+                assertFalse { subject.isCorrect!! }
             }
         }
     }
